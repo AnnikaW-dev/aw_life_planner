e6ff4603bb8739e7c2a8a4eb1ffb743b02175fbd
AnnikaW-dev <warmannika@gmail.com>
2025-08-12 17:28:02 +0200

    Create Shop models & register shop models in admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Category, Module, UserModule
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'friendly_name']
+
+
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'category',
+        'module_type',
+        'price',
+    ]
+    list_filter = [
+        'category',
+        'module_type',
+    ]
+
+
+@admin.register(UserModule)
+class UserModuleAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'module',
+        'purchase_date',
+    ]
+    list_filter = [
+        'module',
+        'purchase_date',
+    ]
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True,blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Module(models.Model):
+    MODULE_TYPES = [
+        ('meal_planner', 'Meal Planner'),
+        ('cleaning_schedule', 'Cleaning Schedule'),
+        ('Stickers', 'Digital Stickers'),
+        ('habit_tracker', 'Habit Tracker'),
+    ]
+
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    module_type = models.CharField(max_length=50, choices=MODULE_TYPES)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    images = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class UserModule(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['user', 'module']
+
+    def __str__(self):
+        return f'{self.user.username} - {self.module.name}'
