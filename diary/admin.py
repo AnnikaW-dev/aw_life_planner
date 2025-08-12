b13b30dc0a27f0719a69098020387e28518c8292
AnnikaW-dev <warmannika@gmail.com>
2025-08-12 13:04:37 +0200

    Create Diary models,Register diary in admin

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import DiaryEntry
 
-# Register your models here.
+
+@admin.register(DiaryEntry)
+class DiaryEntryAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        'date',
+        'title',
+        'mood',
+        'created_at',
+    ]
+    list_filter = [
+        'mood',
+        'created_at',
+        'date',
+    ]
+    seach_fields =[
+        'title',
+        'content',
+        'user__username',
+    ]
+    date_hierarchy = 'date'
--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class DiaryEntry(models.Model):
+    MOOD_CHOICES = [
+        ('happy', '😊 Happy'),
+        ('sad', '😢 Sad'),
+        ('neutral', '😐 Neutral'),
+        ('excited', '🤩 Excited'),
+        ('anxious', '😰 Anxious'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(default=timezone.now)
+    title = models.CharField(max_length=200)
+    content = models.CharField()
+    mood = models.CharField(max_length=50, choices=MOOD_CHOICES,null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering =[
+            '-date',
+            '-created_at',
+        ]
+
+    def __str__(self):
+        return f"{self.user.username} - {self.date}"
