4f5421dee29290315a123dbb7d078e0319bf9117
AnnikaW-dev <warmannika@gmail.com>
2025-08-18 15:57:16 +0200

    Fix settings to be able to se stylings when deploy on Heroku

--- aw_life_planner/settings.py
+++ aw_life_planner/settings.py
@@ -177,6 +177,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Add this for production (at the end of settings.py)
+if not DEBUG:
+    # Use WhiteNoise for static files in production
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
